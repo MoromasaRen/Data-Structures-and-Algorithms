63839dbc35947001afe5338ff6e0a29565ddb46f
RenMoromasa <21104187@usc.edu.ph>
2025-09-17 08:46:39 +0800

    Update Variation_1.c

--- Cursor Based/List/Variation_1.c
+++ Cursor Based/List/Variation_1.c
@@ -28,13 +28,19 @@ void display(int L, VHeap V);
 int main (){
 
     VHeap* V;
-    initialize(V);
+    initialize(&V);
 
     return 0;
 }
 
 // Functions
 void initialize(VHeap *V){
+    /*
+    Set avail to the beginning of list
+    Traverse the list and update each cell’s next to create a chain
+    Set last cell’s next to -1
+    */
+
     int i;
     V->avail = 0;
     for(i = V->avail; i < MAX - 1; i++){
@@ -44,6 +50,13 @@ void initialize(VHeap *V){
 }
 
 int allocSpace(VHeap* V){
+    /*
+    Check if there is an available cell in the virtual heap
+    Get the index of the first available cell
+    Update avail to the next available cell
+    Return the index of the allocated cell
+    */
+
     int ret = V->avail;
     if(ret != -1){
         V->avail = V->H[ret].next;
@@ -52,14 +65,27 @@ int allocSpace(VHeap* V){
 }
 
 void deallocSpace(VHeap* V, int index){
-    if(index < MAX - 1 &&  index >= 0){
+    /*
+    Set the next of the deallocated cell to the current avail index
+    Update avail to point to the newly deallocated cell
+    */
+
+    if(index >= 0 && index < MAX){
         V->H[index].next = V->avail;
         V->avail = index;
     }
 }
 
 void insertFirst(int* L, VHeap* V, int elem){
-    int newCell = allocSpace(&V);
+    /*
+    Allocate a new cell
+    Check if allocation was successful
+    Set the element of the new cell
+    Set the next of the new cell to the current list head
+    Update the list head to point to the new cell
+    */
+
+    int newCell = allocSpace(V);
     if(newCell != -1){
         V->H[newCell].elem = elem;
         V->H[newCell].next = *L;
@@ -68,13 +94,22 @@ void insertFirst(int* L, VHeap* V, int elem){
 }
 
 void insertLast(int* L, VHeap* V, int elem){
-    int newCell = allocSpace(&V);
+    /*
+    Allocate a new cell
+    Check if allocation was successful
+    Set the element of the new cell
+    Set the 'next' of the new cell to -1
+    Use a pointer to traverse to the last cell
+    Update next of last cell to new cell
+    */
+    
+    int newCell = allocSpace(V);
     if(newCell != -1){
         V->H[newCell].elem = elem;
         V->H[newCell].next = -1;
         int *trav = L;
         while(*trav != -1){
-            trav = &V->H[newCell].next;
+            trav = &V->H[*trav].next;
         }
         *trav = newCell;
     }
@@ -82,14 +117,29 @@ void insertLast(int* L, VHeap* V, int elem){
 
 void insertSorted(int* L, VHeap* V, int elem){
 
-
+    int newCell = allocSpace(V);
+    if(newCell != -1){
+        V->H[newCell].elem = elem;
+        int *trav = L;
+        while(*trav != -1 && V->H[*trav].elem < elem){
+            trav = &V->H[*trav].next;
+        }
+        V->H[newCell].next = *trav;
+        *trav = newCell;
+    }
 }
 
 void delete(int* L, VHeap* V, int elem){
+    /*
+    Use a pointer to traverse to the cell with the element to delete
+    Update previous cell’s next to point to current cell’s next
+    Deallocate current cell
+    */
+
     int *trav, temp;
     trav = L;
 
-    for(int i = 0; *trav != -1 && V->H[*trav].elem != elem; i++){
+    while(*trav != -1 && V->H[*trav].elem != elem){
         trav = V->H[*trav].next;
     }
     if(*trav != -1){
@@ -101,8 +151,25 @@ void delete(int* L, VHeap* V, int elem){
 
 void deleteAllOccurrence(int* L, VHeap* V, int elem){
 
+    int *trav = L, temp;
+    
+    while(*trav != -1){
+        if(V->H[*trav].elem = elem){
+            temp = *trav;
+            *trav = V->H[temp].next;
+            deallocSpace(V, temp);
+        } else {
+            trav = &V->H[*trav].next;
+        }
+    }
 }
 
 void display(int L, VHeap V){
-    
+    /*
+    Print all cell values in correct order starting from List head until next is -1
+    */
+    for(int i = L; i != -1; V.H[i].next){
+        printf("%d ", V.H[i].elem);
+    }
+    printf("\n");
 }
