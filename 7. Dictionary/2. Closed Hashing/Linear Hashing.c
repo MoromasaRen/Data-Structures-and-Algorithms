c214a5f363da1a908982b8ea0df5ae9db1977b1b
RenMoromasa <21104187@usc.edu.ph>
2025-10-27 23:31:20 +0800

    Added Hashing

--- 7. Dictionary/2. Closed Hashing/Linear Hashing.c
+++ 7. Dictionary/2. Closed Hashing/Linear Hashing.c
@@ -2,4 +2,118 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define 
+#define MAX_SIZE 10
+#define EMPTY -1
+#define DELETED -2
+#define NIL -1
+
+typedef int Dictionary[MAX_SIZE];
+
+void initDictionary(Dictionary Dict);
+int hash(int value);
+void insertDictionary(Dictionary Dict, int data);
+void deleteData(Dictionary Dict, int data);
+bool search(Dictionary Dict, int data); // isMember
+void display(Dictionary Dict); 
+
+int main (){
+    Dictionary Dict;
+    initDictionary(Dict);
+
+    insertDictionary(Dict, 10);
+    insertDictionary(Dict, 20);
+    insertDictionary(Dict, 30);
+    insertDictionary(Dict, 40);
+    insertDictionary(Dict, 50);
+    insertDictionary(Dict, 60);
+    insertDictionary(Dict, 70);
+    insertDictionary(Dict, 80);
+    insertDictionary(Dict, 90);
+    insertDictionary(Dict, 100);
+    
+    display(Dict);
+    
+    bool found = search(Dict, 20);
+    if(found){
+        printf("20 is in the dictionary\n");
+    } else {
+        printf("20 is not found\n");
+    }
+    
+    deleteData(Dict, 10);
+    display(Dict);
+
+    return 0;
+}
+
+void initDictionary(Dictionary Dict){
+    for(int i = 0; i < MAX_SIZE; i++){
+        Dict[i] = EMPTY;
+    }
+}
+
+int hash(int value){
+    return value % MAX_SIZE;
+}
+
+void insertDictionary(Dictionary Dict, int data){
+    int index = hash(data);
+    int start = index;
+
+    do{
+        if(Dict[index] == EMPTY || Dict[index] == DELETED){
+            Dict[index] = data;
+            printf("Inserted %d at index [%d]\n", data, index);
+            return;
+        }
+        index = (index + 1) % MAX_SIZE;
+    } while(index != start);
+
+    printf("Table is full, cannot insert\n");
+}
+
+void deleteData(Dictionary Dict, int data){
+    int index = hash(data);
+    int start = index;
+
+    do {
+        if(Dict[index] == data){
+            Dict[index] = DELETED;
+            printf("Deleted %d from index [%d]\n", data, index);
+            return;
+        }
+        if(Dict[index] == EMPTY){
+            printf("%d not found in dictionary\n", data);
+            return;
+        }
+        index = (index + 1) % MAX_SIZE;
+    } while (index != start);
+    printf("%d not found in dictionary,\n", data);
+}
+
+bool search(Dictionary Dict, int data){
+    int index = hash(data);
+    int start = index;
+
+    do{
+        if(Dict[index] == data){
+            return true;
+        } 
+        if(Dict[index] == EMPTY){
+            return false;
+        }
+        index = (index + 1) % MAX_SIZE;
+    } while (index != start);
+
+    return false;
+}
+
+void display(Dictionary Dict){
+    printf("\n%-8s %-8s\n", "Index", "Elem");
+    for (int i = 0; i < MAX_SIZE; i++) {
+        printf("[%2d]    %-8d\n", i, Dict[i]);
+    }
+    printf("\n");
+}
+
+
--- 7. Dictionary/2. Closed Hashing/Progressive Overflow.c
+++ 7. Dictionary/2. Closed Hashing/Progressive Overflow.c
@@ -23,9 +23,37 @@ void initDictionary(Dictionary *Dict);
 int hash(int value);
 void insertDictionary(Dictionary *Dict, int data);
 void deleteData(Dictionary *Dict, int data);
-bool search(Dictionary Dict, int data);
+bool search(Dictionary Dict, int data); // isMember
 void display(Dictionary Dict); 
 
+
+int main (){
+
+    Dictionary Dict;
+    initDictionary(&Dict);
+
+    insertDictionary(&Dict, 50);
+    insertDictionary(&Dict, 67);
+    insertDictionary(&Dict, 34);
+    insertDictionary(&Dict, 22);
+    insertDictionary(&Dict, 21);
+    insertDictionary(&Dict, 23);
+    insertDictionary(&Dict, 33);
+    insertDictionary(&Dict, 50);
+    insertDictionary(&Dict, 30);
+    insertDictionary(&Dict, 29);
+    insertDictionary(&Dict, 31);
+
+    bool found = search(Dict, 34);
+    if(found){
+        printf("Data Exists\n");
+    } else {
+        printf("Data does not Exist\n");
+    }
+
+    display(Dict);
+    return 0;
+}
 
 void initDictionary(Dictionary *Dict){
     //Primary
@@ -36,7 +64,7 @@ void initDictionary(Dictionary *Dict){
     }
 
     //Secondary
-    for(int i = 0; i < MAX_S; i++){
+    for(int i = MAX_P; i < TOTAL_SIZE; i++){
         Dict->table[i].occupied = false;
         Dict->table[i].elem = EMPTY;
         Dict->table[i].next = i + 1;
@@ -58,21 +86,89 @@ void insertDictionary(Dictionary *Dict, int data){
     if(!Dict->table[index].occupied){
         Dict->table[index].elem = data;
         Dict->table[index].occupied = true;
-        Dict->table[index].next = -1;
+        Dict->table[index].next = NIL;
         printf("Inserted %d at primary [%d].\n", data, index);
+        return;
     }
 
-    
+    int temp = index;
+    while(Dict->table[temp].next != NIL){
+        temp = Dict->table[temp].next;
+    }
+    if(Dict->avail == NIL){
+        printf("No available space.\n");
+        return;
+    }
+
+    int newIndex = Dict->avail;
+    Dict->avail = Dict->table[newIndex].next;
+
+    Dict->table[newIndex].elem = data;
+    Dict->table[newIndex].occupied = true;
+    Dict->table[newIndex].next = NIL;
+    Dict->table[temp].next = newIndex;
+
+    printf("Collision! Inserted %d at secondary [%d], linked from [%d].\n", data, newIndex, temp);
 }
 
 void deleteData(Dictionary *Dict, int data){
+    int index = hash(data);
+    int prev = NIL;
 
+    while(index != NIL && Dict->table[index].elem != data){
+        prev = index;
+        index = Dict->table[index].next;
+    }
+
+    if(index == NIL){
+        printf("%d not found.\n", data);
+        return;
+    }
+
+    printf("Deleting %d from [%d].\n", data, index);
+    Dict->table[index].occupied = false;
+    Dict->table[index].elem = EMPTY;
+
+    if(index >= MAX_P){
+        Dict->table[index].next = Dict->avail;
+        Dict->avail = index;
+    }
+
+    if(prev != NIL){
+        Dict->table[prev].next = Dict->table[index].next;
+    }
 }
 
 bool search(Dictionary Dict, int data){
+    int index = hash(data);
 
+    while(index != NIL){
+        if(Dict.table[index].occupied && Dict.table[index].elem == data){
+            return true;
+        }
+        index = Dict.table[index].next;
+    }
+    return false;
 }
 
 void display(Dictionary Dict){
+    printf("\n%-8s %-8s %-8s %-8s\n", "Index", "Elem", "Next", "Occupied");
+    printf("----Primary----\n");
+    for(int i = 0; i < MAX_P; i++){
+        printf("[%2d]    %-8d %-8d %-8s\n",
+               i,
+               Dict.table[i].elem,
+               Dict.table[i].next,
+               Dict.table[i].occupied ? "true" : "false");
+    }
 
+    printf("\n----Secondary----\n");
+    for(int i = MAX_P; i < TOTAL_SIZE; i++){
+            printf("[%2d]    %-8d %-8d %-8s\n",
+               i,
+               Dict.table[i].elem,
+               Dict.table[i].next,
+               Dict.table[i].occupied ? "true" : "false");
+    }
+        printf("\nAvail = %d\n", Dict.avail);
 }
